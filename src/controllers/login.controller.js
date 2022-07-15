a276b8c8df7cb1efc1d46ad3f45052d399c8fa07
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-15 16:16:33 -0300

    chore: apaga console log

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -7,7 +7,6 @@ const createLogin = async (req, res) => {
     const foundUser = await loginService.findLogin({ where: { email, password } });
     if (!foundUser) return res.status(400).json({ message: 'Invalid fields' });
     const token = createToken(foundUser);
-    console.log(token);
     return res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ error: err });
