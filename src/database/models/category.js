d9ffae8e79f611d4e5cf6aeed29ed2c10c2dfc21
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-15 17:21:56 -0300

    feat: cria a model de category

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,    
+  },
+  {
+    timestamps: false,
+  });
+
+  return Category;
+};
+
+module.exports = Category;
