3545f90c6ecafa339e408e6c569c51fdaa13edec
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-15 10:34:07 -0300

    feat: adiciona o atributo timestamps false para o model de user

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,17 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     image: DataTypes.STRING,
     password: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
   });
 
   return User;
