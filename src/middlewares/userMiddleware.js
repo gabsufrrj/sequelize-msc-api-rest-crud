215cfcfb2acb4db2195d8fae0b43812750ce797c
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-15 10:38:37 -0300

    feat: cria o middleware de usuario

---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,20 @@
+const userMiddleware = (req, res, next) => {
+  const { displayName, email, password } = req.body;
+  const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  if (displayName.length < 8) {
+    return res.status(400).json(
+      { message: '"displayName" length must be at least 8 characters long' },
+    );
+  }
+  if (!regexEmail.test(email)) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json(
+      { message: '"password" length must be at least 6 characters long' },
+    );
+  }
+  next();
+};
+
+module.exports = userMiddleware;
