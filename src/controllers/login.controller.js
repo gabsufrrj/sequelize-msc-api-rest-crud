c7a740f0f6d4ded6309f83eb24750739fb0a90eb
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-15 10:34:59 -0300

    feat: cria o controllers da rota login

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,19 @@
+const loginService = require('../services/login.service');
+const createToken = require('../helpers/createToken');
+
+const createLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await loginService.findLogin({ where: { email, password } });
+    if (!foundUser) return res.status(400).json({ message: 'Invalid fields' });
+    const token = createToken(foundUser);
+    console.log(token);
+    return res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+module.exports = {
+  createLogin,
+};
