77d4908cbfbc14bf0f712b0830dce7c818588eba
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-18 10:11:24 -0300

    feat: cria a associação na model das tabelas users e blogposts

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,29 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreingKey: true,      
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+    { foreingKey: 'userId', as: 'users'})
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
