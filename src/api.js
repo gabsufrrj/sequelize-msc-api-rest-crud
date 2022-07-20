7e0def367df0df41522702aa96cdac3918f4b4e5
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-07-20 18:11:30 -0300

    chore: remove comentários

--- src/api.js
+++ src/api.js
@@ -6,8 +6,6 @@ const loginRouter = require('./routes/login.router');
 const categoryRouter = require('./routes/category.router');
 const blogPostRouter = require('./routes/blogpost.router');
 
-// ...
-
 const app = express();
 
 app.use(express.json());
@@ -22,8 +20,4 @@ app.use('/post', blogPostRouter);
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
